fae14ae31f4e240d29f8eb0b35d48b39ccc5c137
cRui861 <cRui861@users.noreply.github.com>
2017-03-14 21:52:21 -0700

    STILL working on FEED yay

--- snapChatProject/ImageFeedController.swift
+++ snapChatProject/ImageFeedController.swift
@@ -9,29 +9,34 @@
 import Foundation
 import UIKit
 
-class ImageFeedController: UITableView {
+class ImageFeedController: UITableViewController {
     
-    var testingNum = 5
-    //var sectionTitles = ["Memes", "Dog Spots", "Random"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testingNum //this will be changed but is being bitchy
-        // return thread[section].count doesn't work lol 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return threads[threadNames[section]]!.count
     }
     
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return threadNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return threadNames[section]
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "toPicture", for: indexPath) as! imageFeedTableViewCell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "viewFeedCell", for: indexPath) as! imageFeedTableViewCell
         cell.hasRead.image = #imageLiteral(resourceName: "unread")
         cell.whoRead.text = "Ichirou"
         cell.timeRead.text = "1 Minute"
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         
     }
 }
